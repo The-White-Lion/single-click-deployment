4753f53766312d29aa45468581666a5cbb956d5c
lazytiger <whitelion.b@outlook.com>
2022-11-25 18:22:41 +0800

    vim 自动安装插件

--- installation_scripts/vim.sh
+++ installation_scripts/vim.sh
@@ -15,4 +15,7 @@ cp "${TMP_DIR}/config/vim/vimrc" "${HOME}/.vimrc"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-yellow "vim 安装完成，请在 vim 中使用 :PlugInstall 更新插件"
+# Install Plugin
+vim +PlugInstall +qall
+
+yellow "vim 安装完成"
