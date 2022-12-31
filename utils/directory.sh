87826b9b4b65be6f067542e84cf2608267898b72
ShuBo6 <814183583@qq.com>
2022-12-31 13:07:09 +0800

    fix: directory.sh mkdir -p 参数

--- utils/directory.sh
+++ utils/directory.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-[[ ! -d "${CONFIG_DIR}" ]] && mkdir "${CONFIG_DIR}"
+[[ ! -d "${CONFIG_DIR}" ]] && mkdir -p "${CONFIG_DIR}"
 
-[[ ! -d "${GITHUB_DIR}" ]] && mkdir "${GITHUB_DIR}"
+[[ ! -d "${GITHUB_DIR}" ]] && mkdir -p "${GITHUB_DIR}"
 
-[[ ! -d "${BIN_DIR}" ]] && mkdir "${BIN_DIR}"
+[[ ! -d "${BIN_DIR}" ]] && mkdir -p "${BIN_DIR}"
 
-[[ ! -d "${DEV_ENV_DIR}" ]] && mkdir "${DEV_ENV_DIR}"
+[[ ! -d "${DEV_ENV_DIR}" ]] && mkdir -p "${DEV_ENV_DIR}"
 
-[[ ! -d "${CODE_DIR}" ]] && mkdir "${CODE_DIR}"
+[[ ! -d "${CODE_DIR}" ]] && mkdir -p "${CODE_DIR}"
 
-[[ ! -d "${TMP_DIR}" ]] && mkdir "${TMP_DIR}"
+[[ ! -d "${TMP_DIR}" ]] && mkdir -p "${TMP_DIR}"
 
-[[ ! -d "${ZSH_DIR}" ]] && mkdir "${ZSH_DIR}"
+[[ ! -d "${ZSH_DIR}" ]] && mkdir -p "${ZSH_DIR}"
 
-[[ ! -d "${FONT_DIR}" ]] && mkdir "${FONT_DIR}"
+[[ ! -d "${FONT_DIR}" ]] && mkdir -p "${FONT_DIR}"
