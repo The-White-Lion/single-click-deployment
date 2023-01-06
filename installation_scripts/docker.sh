12e42496b7c88a848d15e2ed64852109ea7e69d1
lazytiger <whitelion.b@outlook.com>
2023-01-06 20:21:59 +0800

    ubuntn install docker-compose

--- installation_scripts/docker.sh
+++ installation_scripts/docker.sh
@@ -17,7 +17,7 @@ function ubuntu() {
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 }
 
 function fedora() {
